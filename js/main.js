eb5212169025f18eccf0c286aac3eae693b92e17
Zakirul Islam <zakirul011@gmail.com>
2025-08-25 22:51:40 +0600

    added rest of the section without footer

--- js/main.js
+++ js/main.js
@@ -13,3 +13,30 @@ document.addEventListener("DOMContentLoaded", function(){
     });
   }
 });
+
+
+// swiper
+const swiper = new Swiper('.projects__slider', {
+  slidesPerView: 1,
+  breakpoints: {
+    768: { 
+      slidesPerView: 2
+    },
+    1200: { 
+      slidesPerView: 3 
+    }
+  },
+  loop: true,
+
+  spaceBetween: 50,
+  
+  navigation: {
+    nextEl: '.projects .swiper-button-next',
+    prevEl: '.projects .swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.projects .swiper-pagination',
+    clickable: true,
+  },
+});
