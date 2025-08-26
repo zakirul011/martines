76bba74e8072ead82b16de258e8e5543bdafcb71
Zakirul Islam <zakirul011@gmail.com>
2025-08-26 20:56:13 +0600

    separate the reponsive css and fix the offset menu

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
 
-// dropdown on hover for large screens
-document.addEventListener("DOMContentLoaded", function(){
-  if (window.innerWidth > 991) { // only for large screens
-    document.querySelectorAll('.navbar .dropdown').forEach(function(e){
-        const dropdown = bootstrap.Dropdown.getOrCreateInstance(e.querySelector('.dropdown-toggle'));
-        e.addEventListener('mouseenter', function(){
-            dropdown.show();
-        });
-        e.addEventListener('mouseleave', function(){
-            dropdown.hide();
-        });
-    });
-  }
-});
-
 
 // swiper
 const swiper = new Swiper('.projects__slider', {
