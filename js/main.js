c167f4cb69ff7c76498bf677f46bb4299f06035b
Zakirul Islam <zakirul011@gmail.com>
2025-08-28 16:34:20 +0600

    update the animation in mobile

--- js/main.js
+++ js/main.js
@@ -18,10 +18,14 @@ animationStagger.forEach(w => {
 window.addEventListener('scroll', ()=>{
   animations.forEach(el => {
     let t = el.getBoundingClientRect().top - window.innerHeight * 3 / 4
-    if (t <= 0 && window.innerWidth >= 768) {
+    if (window.innerWidth >= 768) {
       el.classList.add('animation-added')
-    }else{      
-      el.classList.remove('animation-added')
+    }else{
+      if (t <= 0) {
+        el.classList.add('animation-added')
+      }else{      
+        el.classList.remove('animation-added')
+      }
     }
   });
 })
